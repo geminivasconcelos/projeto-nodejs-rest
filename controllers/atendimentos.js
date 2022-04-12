e984ec6e67a71c496a440bdcde90e51b9fff4510
Ge vasconcelos <geminivasconceelos@gmail.com>
2022-04-11 21:38:39 -0300

    Refactor - arquivos certos

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,8 +1,11 @@
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos'));
+    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'))
 
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body);
-        res.send('Você está na rota de atendimento e esta realizando um post')
-    });
+        console.log('Atendimento enviado')
+        console.log(req.body)
+        res.send('Post atendimento')
+    })
+     
+     
 }
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const customExpress = require('./config/customExpress');
+const customExpress = require('./config/customExpress')
 
-const app = customExpress();
+const app = customExpress()
 
-app.listen(3000, () => console.log('servidor rodando na porta 3000'));
+app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
